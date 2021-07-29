7beb98ebe0c50904d6d0e6f7441dab87825df32a
Dexshine <b_o_y_s@hotmail.com>
2021-07-29 14:13:10 +0700

    author section

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ const color = document.querySelector('#color')
 
 btn_toggle.onclick = () => {
   // your code here
+  var id = 620610786
+  console.log(author.innerHTML.length)
+  if(author.innerHTML.length > 10){
+    author.innerHTML = id + parseInt(length.value)
+  }else{
+    author.innerHTML = '620610786 NATTAPON TANCHO'
+  }
 }
 
 // more codes for Search and Reset buttons here
